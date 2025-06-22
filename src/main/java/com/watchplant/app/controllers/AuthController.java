a4dfcad9dca6a88aeabaa32fd5d90f7395f78a92
Raphael <r.leiva@unesp.br>
2025-06-22 15:28:51 -0300

    chore: validate frontend url in CORS config

--- src/main/java/com/watchplant/app/controllers/AuthController.java
+++ src/main/java/com/watchplant/app/controllers/AuthController.java
@@ -6,10 +6,7 @@ import com.watchplant.app.dtos.auth.JwtTokenResponseDTO;
 import com.watchplant.app.services.AuthService;
 import com.watchplant.app.services.exceptions.ApplicationException;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
